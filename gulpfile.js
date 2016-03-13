a901663a5f7ca100efc7c70475653fc9881b2e6e
Srinivas <uisrinivas@gmail.com>
2016-03-13 22:51:43 +0530

    regular task
    Adding more task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,33 @@
 var gulp=require('gulp');
-var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+
 gulp.task('script',function(){
 	return gulp.src('public/js/**/*.js')
+	.pipe(sourcemaps.init())
+	.pipe(babel({
+		presets: ['es2015']
+	}))
 	.pipe(concat('all.js'))
 	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('dist/js'))
 })
 
-gulp.task('default',['script'])
+gulp.task('minify', function() {
+  return gulp.src('public/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('image',function(){
+	return gulp.src('public/images/*')
+	.pipe(imagemin({
+		progressive: true
+	}))
+	.pipe(gulp.dest('dist/images'));
+})
+
+gulp.task('default',['script','minify','image'])
